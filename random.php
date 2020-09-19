f4cbc315cbb7eabb205348166385ea55c043488f
Slienca <Slienca@users.noreply.github.com>
2020-09-20 00:57:24 +0800

    data

--- random.php
+++ random.php
@@ -1,33 +1,8 @@
 <?php
-//存有美图链接的文件名img.txt
-$filename = "img.txt";
-if(!file_exists($filename)){
-    die('文件不存在');
-}
- 
-//从文本获取链接
-$pics = [];
-$fs = fopen($filename, "r");
-while(!feof($fs)){
-    $line=trim(fgets($fs));
-    if($line!=''){
-        array_push($pics, $line);
-    }
-}
- 
-//从数组随机获取链接
-$pic = $pics[array_rand($pics)];
- 
-//返回指定格式
-$type=$_GET['type'];
-switch($type){
- 
-//JSON返回
-case 'json':
-    header('Content-type:text/json');
-    die(json_encode(['pic'=>$pic]));
- 
-default:
-    die(header("Location: $pic"));
-}
+
+$rand = rand(1,201);
+$url = 'https://cdn.jsdelivr.net/gh/Slienca/Imgur/Images/'.$rand.'.jpg';
+$img = file_get_contents($url,true);
+header("Content-Type: image/jpeg;");
+echo $img;
 ?>
