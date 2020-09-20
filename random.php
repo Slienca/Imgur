0920309f858a9227406f32ab86200220448883b2
Slienca <Slienca@users.noreply.github.com>
2020-09-20 09:33:04 +0800

    data

--- random.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$rand = rand(1,201);
-$url = 'https://cdn.jsdelivr.net/gh/Slienca/Imgur/Images/'.$rand.'.jpg';
-$img = file_get_contents($url,true);
-header("Content-Type: image/jpeg;");
-echo $img;
-?>
